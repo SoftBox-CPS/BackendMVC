cce9ec8e8b11575795642045ed2937fe8b2b3329
Тимофей И <iwanov.timofei2001@yandex.ru>
2022-05-04 17:13:00 +0300

    Add base repository

---
+++ SoftBox.DataBase/InterfacesRepository/Base/INamedRepository.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SoftBox.DataBase.InterfacesRepository.Base
+{
+    public interface INamedRepository<T, B> : IRepository<T, B> where T : InterfacesEntities.INamedEntities<B>
+    {
+
+        Task<T> GetByName(string name, CancellationToken cancel = default);
+
+    }
+}
---
+++ SoftBox.DataBase/Repository/ChatRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftBox.DataBase.Repository
+{
+    public class ChatRepository : Base.DbNamedRepository<Entities.Chat, Guid>, InterfacesRepository.IChatRepository
+    {
+        public ChatRepository(SoftBoxDbContext db) : base(db)
+        {
+        }
+    }
+}
