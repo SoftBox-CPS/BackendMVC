a892fa7a421f7f6035197e20c0784505b4d29ed9
Тимофей И <iwanov.timofei2001@yandex.ru>
2022-05-17 16:49:05 +0300

    Change format namespaces

--- SoftBox.DataBase/Enum/OrderBy.cs
+++ SoftBox.DataBase/Enum/OrderBy.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace SoftBox.DataBase.Enum;
 
-namespace SoftBox.DataBase.Enum
+public enum OrderBy
 {
-    public enum OrderBy
-    {
-        None,
-        PriceAsc,
-        PriceDesc,
-        DataDesc
-    }
+    None,
+    PriceAsc,
+    PriceDesc,
+    DataDesc
 }
