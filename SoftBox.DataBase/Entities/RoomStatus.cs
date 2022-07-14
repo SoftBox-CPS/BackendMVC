adb63dfa54543f535bc127e12fbc2b42836ac8d7
I'm the really stupid guy <romankartasheff@gmail.com>
2022-07-15 00:09:20 +0300

    change EntityName to Entity

--- SoftBox.DataBase/Entities/RoomStatus.cs
+++ SoftBox.DataBase/Entities/RoomStatus.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace SoftBox.DataBase.Entities;
 
 [Table("room_status")]
-public class RoomStatus : Base.EntityName<int>
+public class RoomStatus : Base.Entity<int>
 {
     public string Title { get; set; }
     public ICollection<Room> Rooms { get; set; }
