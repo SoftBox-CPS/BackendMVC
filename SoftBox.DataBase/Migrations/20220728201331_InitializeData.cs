442d3dc0763e9df9eb2468cda053a09e5876c81e
tanyagaKim <tatchuikova@gmail.com>
2022-07-28 23:14:29 +0300

    Seed initiall data to database with migration

---
+++ SoftBox.DataBase/Migrations/20220728201331_InitializeData.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftBox.DataBase.Migrations
+{
+    public partial class InitializeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "id", "description", "image_url", "name" },
+                values: new object[,]
+                {
+                    { 1, "Аптека", null, "Аптека" },
+                    { 2, "Книги", null, "Книги" },
+                    { 3, "Одежда", null, "Одежда" },
+                    { 4, "Обувь", null, "Обувь" },
+                    { 5, "Техника", null, "Техника" },
+                    { 6, "Мебель", null, "Мебель" },
+                    { 7, "Спорт и отдых", null, "Спорт и отдых" },
+                    { 8, "Хобби и творчество", null, "Хобби и творчество" },
+                    { 9, "Для школы и офиса", null, "Для школы и офиса" },
+                    { 10, "Для дома", null, "Для дома" },
+                    { 11, "Зоотовары", null, "Зоотовары" },
+                    { 12, "Юверирные украшения", null, "Юверирные украшени" },
+                    { 13, "Оборудование", null, "Оборудование" },
+                    { 14, "Детские товары", null, "Детские товары" },
+                    { 15, "Дача и сад", null, "Дача и сад" },
+                    { 16, "Строительство и ремонт", null, "Строительство и ремонт" },
+                    { 17, "Красота и гигиена", null, "Красота и гигиена" },
+                    { 18, "Электроника", null, "Электроника" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "room_user_types",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 1, "Владелец" },
+                    { 2, "Пользователь" },
+                    { 3, "Модератор" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "user_organization_types",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 1, "Организатор" },
+                    { 2, "Директор" },
+                    { 3, "Представитель" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "user_types",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 1, "admin" },
+                    { 2, "user" },
+                    { 3, "moderator" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "room_user_types",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "room_user_types",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "room_user_types",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "user_organization_types",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "user_organization_types",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "user_organization_types",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "user_types",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "user_types",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "user_types",
+                keyColumn: "id",
+                keyValue: 3);
+        }
+    }
+}
--- SoftBox.DataBase/SoftBoxDbContext.cs
+++ SoftBox.DataBase/SoftBoxDbContext.cs
@@ -31,7 +31,7 @@ public class SoftBoxDbContext : DbContext
 
         addGenerateGuidToId(modelBuilder);
 
-        SeedData.Initialize(modelBuilder);
+       Seed(modelBuilder);
     }
 
     private void addGenerateGuidToId(ModelBuilder modelBuilder)
@@ -44,5 +44,48 @@ public class SoftBoxDbContext : DbContext
         }
 
     }
+
+	private void Seed(ModelBuilder modelBuilder)
+	{
+		// Seeds table of user_types
+		modelBuilder.Entity<UserType>().HasData(
+			new UserType { Id = 1, Name = "admin" },
+			new UserType { Id = 2, Name = "user" },
+			new UserType { Id = 3, Name = "moderator" });
+
+		// Seeds table of user_organization_types
+		modelBuilder.Entity<UserOrganizationType>().HasData(
+			new UserOrganizationType { Id = 1, Name = "Организатор" },
+			new UserOrganizationType { Id = 2, Name = "Директор" },
+			new UserOrganizationType { Id = 3, Name = "Представитель" });
+
+		// Seeds table of user_room_types
+		modelBuilder.Entity<RoomUserType>().HasData(
+			new RoomUserType { Id = 1, Name = "Владелец" },
+			new RoomUserType { Id = 2, Name = "Пользователь" },
+			new RoomUserType { Id = 3, Name = "Модератор" });
+
+		// Seeds table of categories
+		modelBuilder.Entity<Category>().HasData(
+			new Category { Id = 1, Name = "Аптека", Description = "Аптека" },
+			new Category { Id = 2, Name = "Книги", Description = "Книги" },
+			new Category { Id = 3, Name = "Одежда", Description = "Одежда" },
+			new Category { Id = 4, Name = "Обувь", Description = "Обувь" },
+			new Category { Id = 5, Name = "Техника", Description = "Техника" },
+			new Category { Id = 6, Name = "Мебель", Description = "Мебель" },
+			new Category { Id = 7, Name = "Спорт и отдых", Description = "Спорт и отдых" },
+			new Category { Id = 8, Name = "Хобби и творчество", Description = "Хобби и творчество" },
+			new Category { Id = 9, Name = "Для школы и офиса", Description = "Для школы и офиса" },
+			new Category { Id = 10, Name = "Для дома", Description = "Для дома" },
+			new Category { Id = 11, Name = "Зоотовары", Description = "Зоотовары" },
+			new Category { Id = 12, Name = "Юверирные украшени", Description = "Юверирные украшения" },
+			new Category { Id = 13, Name = "Оборудование", Description = "Оборудование" },
+			new Category { Id = 14, Name = "Детские товары", Description = "Детские товары" },
+			new Category { Id = 15, Name = "Дача и сад", Description = "Дача и сад" },
+			new Category { Id = 16, Name = "Строительство и ремонт", Description = "Строительство и ремонт" },
+			new Category { Id = 17, Name = "Красота и гигиена", Description = "Красота и гигиена" },
+			new Category { Id = 18, Name = "Электроника", Description = "Электроника" });
+	}
+
 
 }
